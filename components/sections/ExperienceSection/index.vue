6cb4013e9a9d1c074fbb9c96f2b64ae341e5dcd7
bncpiner <bncpiner@vauldex.com>
2025-04-28 08:44:48 +0800

    Update components ui

--- components/sections/ExperienceSection/index.vue
+++ components/sections/ExperienceSection/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <section class="p-8 w-full text-white min-h-screen flex flex-col gap-8">
+  <section class="p-8 w-full text-white min-h-screen space-y-8 relative">
     <LazySectionTitle :title="experience.title" :description="experience.description" />
     <LazyExperienceList :experiences="experience.experiences"/>
+
+    <!-- DECORATIVE -->
+    <div class="absolute top-1/2 left-1/2 bg-violet-600/10 rounded-full h-[600px] w-[600px] -translate-y-1/4 -translate-x-1/2 -z-10 blur-3xl will-change-[filter]" />
   </section>
 </template>
 
