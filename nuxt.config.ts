63679780f616810797df1deb1b2a8b6053f84cce
bncpiner <bncpiner@vauldex.com>
2025-04-28 09:01:21 +0800

    Add meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
+  app: {
+    head: {
+      title: `Brill's Portfolio`,
+      meta: [
+        { name: 'description', content: 'This is my amazing portfolio using Nuxt 3 framework.' },
+        { property: 'og:image', content: '/images/portfolio-brillnash-vercel.png' },
+        { property: 'og:url', content: 'https://portfolio-brillnash.vercep.app' },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+    },
+  },
   compatibilityDate: '2025-04-24',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
