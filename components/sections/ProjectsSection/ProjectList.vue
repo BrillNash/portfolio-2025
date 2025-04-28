92f1c083af54b6c12385677375ceddf115efae0f
bncpiner <bncpiner@vauldex.com>
2025-04-28 08:44:07 +0800

    Add ProjectsSection

---
+++ components/sections/ProjectsSection/ProjectList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 items-center justify-center">
+    <ProjectCard v-for="project in props.projects" :key="project.name" :project="project"/>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import type { ProjectItem } from '~/constants/types';
+
+type Props = {
+  projects: ProjectItem[]
+}
+
+const props = defineProps<Props>()
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <LandingSection />
+    <LazyProjectsSection />
     <LazyServicesSection />
     <LazyExperienceSection />
     <LazySkillsSection />
