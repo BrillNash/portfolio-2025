c2d74525d5d284b8ddbbe3d8e9b67d1dc395dc22
bncpiner <bncpiner@vauldex.com>
2025-04-30 11:13:57 +0800

    Add portfolio icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,13 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/atom.png'
+        }
+      ]
     },
   },
   compatibilityDate: '2025-04-24',
