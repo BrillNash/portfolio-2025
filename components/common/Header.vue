7bd254c1650e6b3eae88fef65aab5b6fc2207683
bncpiner <bncpiner@vauldex.com>
2025-04-30 10:35:20 +0800

    Add responsive burger

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="fixed left-0 top-0 z-50 w-full bg-slate-950 border-b-gray-700 p-4">
-    <ul class="flex items-center justify-center gap-8 py-4 text-white font-bold text-sm tracking-wide transition-colors">
+    <ul class="hidden sm:flex items-center justify-center gap-8 py-4 text-white font-bold text-sm tracking-wide transition-colors">
       <li 
         v-for="item in headerItems" 
         :class="[
@@ -13,7 +13,40 @@
         <span>{{ item.label }}</span>
       </li>
     </ul>
+    <div class="flex sm:hidden text-white justify-between items-center">
+      <h2>Nash</h2>
+      <UButton
+        class="text-white"
+        size="xl"
+        icon="uil:bars"
+        variant="link"
+        @click="isDrawerOpen = !isDrawerOpen"
+      />
+    </div>
   </nav>
+  <UDrawer 
+    direction="left" 
+    v-model:open="isDrawerOpen" 
+    :handle="false"
+    class="bg-slate-900 ring-none"
+    :ui="{ overlay: 'bg-slate-950/30' }"
+  >
+    <template #content>
+      <ul class="flex flex-col items-center justify-start gap-8 py-4 text-white font-bold text-2xl tracking-wide transition-colors w-screen mt-16">
+        <li 
+          v-for="item in headerItems" 
+          :class="[
+            'relative cursor-pointer space-x-1 py-2 px-4 flex items-center justify-center',
+            { 'bg-linear-to-r from-violet-500 to-cyan-600 shadow-violet-500/30 shadow-[0_0_10px_10px]': activeSection === item.id }
+          ]" 
+          @click="scrollToSection(item.id)"
+        >
+          <UIcon :name="item.icon" class="size-5" />
+          <span>{{ item.label }}</span>
+        </li>
+      </ul>
+    </template>
+  </UDrawer>
 </template>
 
 <script lang="ts" setup>
@@ -21,6 +54,7 @@ const { headerItems } = await import('@/constants/content.js')
 const activeSection = ref('home');
 const sectionIds = ['home', 'projects', 'services', 'experience', 'skills'];
 const observers: IntersectionObserver[] = [];
+const isDrawerOpen = ref(false)
 
 const scrollToSection = (id: string) => {
   const el = document.getElementById(id);
@@ -42,7 +76,7 @@ const intersectionObserver = (el: HTMLElement, id: string) => {
     {
       root: null, // viewport
       rootMargin: '0px',
-      threshold: 0.5, // 50% of the section must be visible
+      threshold: 0.1, // 50% of the section must be visible
     }
   );
   observer.observe(el);
