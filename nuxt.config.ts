0df8d909f0327202c690841c7a0d687a659569d6
bncpiner <bncpiner@vauldex.com>
2025-04-30 14:54:33 +0800

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/fonts'],
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/image'],
   app: {
     head: {
       title: `Brill's Portfolio`,
