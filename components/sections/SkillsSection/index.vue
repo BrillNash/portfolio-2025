22a6dfcc27c9b6990e8247bacc0b8ce569af9444
BrillNash <brillnash.piner@gmail.com>
2025-05-05 01:40:22 +0800

    Change text color in tabs

--- components/sections/SkillsSection/index.vue
+++ components/sections/SkillsSection/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="p-5 sm:p-8 text-white min-h-screen w-full space-y-8 h-full">
     <LazySectionTitle :title="skills.title" :description="skills.description"/>
-    <UTabs :items="tabItems" class="w-full" :ui="{ list: 'bg-black', indicator: 'bg-violet-600/40' }">
+    <UTabs :items="tabItems" class="w-full" :ui="{ list: 'bg-black', indicator: 'bg-violet-600/40', label: 'text-white' }">
       <template #content="{ item }">
         <LazySkillList :skills="skills.skills.filter((skill) => skill.group.includes(item.label))" :group="item.label" />
       </template>
