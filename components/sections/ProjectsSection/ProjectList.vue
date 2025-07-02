e287509405bc3a626322fc40867cfdd113acbefa
BrillNash <brillnash.piner@gmail.com>
2025-07-02 20:49:47 +0800

    add ssr html tags for seo in projects page

--- components/sections/ProjectsSection/ProjectList.vue
+++ components/sections/ProjectsSection/ProjectList.vue
@@ -1,7 +1,11 @@
 <template>
-  <ul class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 items-center justify-center">
-    <ProjectCard v-for="project in props.projects" :key="project.name" :project="project"/>
-  </ul>
+  <section aria-labelledby="projects-heading" class="w-full">
+    <h2 id="projects-heading" class="sr-only">Brill Nash Piner's Project List</h2>
+    <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 items-center justify-center">
+      <ProjectCard v-for="project in props.projects" :key="project.name" :project="project"/>
+    </ul>
+  </section>
+  
 </template>
 
 <script lang="ts" setup>
--- components/sections/ProjectsSection/index.vue
+++ components/sections/ProjectsSection/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <section class="p-5 sm:p-8 w-full text-white min-h-screen space-y-8 relative">
-    <SectionTitle :title="projects.title" :description="projects.description"/>
-    <ProjectList :projects="projects.projects"/>
+  <section aria-labelledby="projects-section" class="p-5 sm:p-8 w-full text-white min-h-screen space-y-8 relative">
+    <h2 id="projects-section" class="sr-only">Brill Nash Piner's Projects</h2>
+
+    <LazySectionTitle :title="projects.title" :description="projects.description"/>
+    <LazyProjectList :projects="projects.projects"/>
   </section>
 </template>
 
 <script lang="ts" setup>
 const { projects } = await import("~/constants/content.js")
-</script>
